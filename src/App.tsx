d3f026852d5ce929e1e7f360a007b98b3bbb7704
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2022-06-22 20:05:20 +0500

    Немного рефакторинга

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import styles from "./App.less";
 import cn from "classnames";
 
-const width = 8;
-const height = 10;
-const countMines = 10;
+const width = 14;
+const height = 18;
+const countMines = 40;
 
-type tale = {
+type tile = {
     x: number,
     y: number,
     flag: boolean,
@@ -15,7 +15,7 @@ type tale = {
     countMineAround: number
 }
 
-const addMinesOnMap = (arr: tale[][], x: number, y: number) => {
+const addMinesOnMap = (arr: tile[][], x: number, y: number) => {
     const addMine = (x: number, y: number) => {
         arr[x][y].mine = true;
         arr[x][y].countMineAround = -1;
@@ -28,26 +28,20 @@ const addMinesOnMap = (arr: tale[][], x: number, y: number) => {
         }
     }
 
-    const temp = arr.flat();
-    console.log(temp)
-    console.log(x)
-    console.log(y)
-    const index = x * height + y; //temp.findIndex(element => element.x === x && element.y === y)
-    temp.splice(index, 1);
-    console.log(index)
-    console.log(temp)
+    const flat = arr.flat();
+    const index = x * height + y;
+    flat.splice(index, 1);
 
-    while (width * height - temp.length - 1 !== countMines) {
-        const index = Math.floor(Math.random() * temp.length);
-        const element = temp[index];
-        console.log(temp[index])
+    while (width * height - flat.length - 1 !== countMines) {
+        const index = Math.floor(Math.random() * flat.length);
+        const element = flat[index];
         addMine(element.x, element.y);
-        temp.splice(index, 1);
+        flat.splice(index, 1);
     }
 }
 
 const getMap = () => {
-    const arr: tale[][] = [];
+    const arr: tile[][] = [];
 
     for (let x = 0; x < width; x++) {
         arr[x] = [];
@@ -60,27 +54,17 @@ const getMap = () => {
 }
 
 function bfs(map: any, x: number, y: number) {
-    // adj - смежный список
-    // map - начальная вершина
-    // t - пункт назначения
-    // инициализируем очередь
-    let queue = []
-    // добавляем map в очередь
+    const queue = []
     if (!map[x][y].flag) {
         queue.push(map[x][y])
         map[x][y].opened = true
     }
     while (queue.length > 0) {
-        // удаляем первый (верхний) элемент из очереди
-        let v = queue.shift()
-        // abj[v] - соседи v
+        const v = queue.shift()
         if (v.countMineAround === 0)
             for (let dx = Math.max(v.x - 1, 0); dx < Math.min(v.x + 2, width); dx++) {
                 for (let dy = Math.max(v.y - 1, 0); dy < Math.min(v.y + 2, height); dy++) {
                     if (!map[dx][dy].opened && !map[dx][dy].flag) {
-                        // добавляем его в очередь
-                        // помечаем вершину как посещенную
-                        // если сосед является пунктом назначения, мы победили
                         map[dx][dy].opened = true
                         if (map[dx][dy].countMineAround === 0) {
                             queue.push(map[dx][dy]);
@@ -90,11 +74,10 @@ function bfs(map: any, x: number, y: number) {
             }
     }
     return map
-    // если t не обнаружено, значит пункта назначения достичь невозможно
 }
 
 function App() {
-    const [map, setMap] = useState<tale[][]>([]);
+    const [map, setMap] = useState<tile[][]>([]);
     const [firstStep, setFirstStep] = useState(true);
 
     if (!map.length)
@@ -117,28 +100,26 @@ function App() {
 
     return (
         <div className={styles.App}>
-            <div>
-                {
-                    map.map(line =>
-                        <div className={styles.LineMinesweeper}>
-                            {
-                                line.map(el => <div className={cn(styles.TileMinesweeper, {
-                                    [styles.Flag]: el.flag,
-                                    [styles.Mine]: el.mine && el.opened,
-                                    [styles.Opened]: el.opened
-                                })}
-                                                    onClick={(e) => onClick(e, el.x, el.y)}
-                                                    onContextMenu={(e) => onClick(e, el.x, el.y)}
-                                >
-                                    {
-                                        el.opened && (el.mine ? "MINE" : el.countMineAround > 0 && el.countMineAround)
-                                    }
-                                </div>)
-                            }
-                        </div>
-                    )
-                }
-            </div>
+            {
+                map.map(line =>
+                    <div className={styles.LineMinesweeper}>
+                        {
+                            line.map(el => <div className={cn(styles.TileMinesweeper, {
+                                [styles.Flag]: el.flag,
+                                [styles.Mine]: el.mine && el.opened,
+                                [styles.Opened]: !el.mine && el.opened
+                            })}
+                                                onClick={(e) => onClick(e, el.x, el.y)}
+                                                onContextMenu={(e) => onClick(e, el.x, el.y)}
+                            >
+                                {
+                                    el.opened && (el.mine ? "MINE" : el.countMineAround > 0 && el.countMineAround)
+                                }
+                            </div>)
+                        }
+                    </div>
+                )
+            }
         </div>
     );
 }
